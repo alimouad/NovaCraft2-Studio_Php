2d8ec8c90696874207bfb1db4ff3504bba4604fc
mouadali <mouadalli311@gmail.com>
2025-12-18 21:44:50 +0100

    update some files

--- app/views/account.php
+++ app/views/account.php
@@ -3,14 +3,21 @@
 if (isset($_SESSION["user_id"])) {
     
     $mysqli = require __DIR__ . "/../../config/database.php";
-    
-    $sql = "SELECT * FROM users
-            WHERE id = {$_SESSION["user_id"]}";
-            
-    $result = $mysqli->query($sql);
-    
-    // associative_array
+
+    $sql = "SELECT * FROM users WHERE id = ?";
+    $stmt = $mysqli->prepare($sql);
+
+    $stmt->bind_param("i", $_SESSION["user_id"]);
+
+    $stmt->execute();
+    $result = $stmt->get_result();
+    // return data in a associative array format
     $user = $result->fetch_assoc();
+    
+    if (!$user) {
+        header("Location: /logout");
+        exit;
+    } 
 }
 
 ?>
