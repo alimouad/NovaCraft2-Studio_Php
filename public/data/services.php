df2cbcbd7139fd2c8f9738ec3ca9028984e03492
mouadali <mouadalli311@gmail.com>
2025-12-18 09:17:17 +0100

    add index.php

---
+++ public/data/services.php
@@ -0,0 +1,10 @@
+<?php 
+
+$services = array(
+    "Création de sites Web"=>"Sites vitrines, e-commerce, applications modernes.",
+    "Développement sur mesure" =>"Outils internes, solutions métiers spécifiques.",
+    "Automatisation & API"=>"Automatisation des tâches, intégration d'API.",
+    "Développement sur mesur" =>"Outils internes, solutions métiers spécifiques.",
+    "Développement sur mesura" =>"Outils internes, solutions métiers spécifiques."
+)
+?>
---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+require __DIR__ . "/../router/router.php";
+
+
+// 2. Include core classes globally
+require_once __DIR__ . '/../app/Core/Auth.php';
+
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+route($uri);
+
+?>
